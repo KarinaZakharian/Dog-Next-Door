02c89075ce89dc9dcca125f7736d5e3304b7fb48
KarinaZakharian <karina.zakharian@oclock.school>
2023-09-21 22:52:10 +0200

    type/button tsx and scss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
 
 
+import Button from '../InputType/Button/Button';
 import './App.scss';
 
 function App() {
  
- 
+  return (
+    <div className="App">
+     
+    </div>
+  );
 }
 
 export default App;
