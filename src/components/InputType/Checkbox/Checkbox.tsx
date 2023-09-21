71732aa51b6346f62235e1a0f9b436e712d0bb1a
KarinaZakharian <karina.zakharian@oclock.school>
2023-09-21 22:55:13 +0200

    type/checkbox tsx and scss

---
+++ src/components/InputType/Checkbox/Checkbox.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable prettier/prettier */
+/* <CheckboxGroup options={options} legend="Check box" /> */
+/* const options = ['Option 1', 'Option 2', 'Option 3', 'Option 4']; */
+
+import { useState } from 'react';
+import './Checkbox.scss';
+
+interface CheckboxGroupProps {
+  options: string[];
+  legend: string;
+}
+
+function CheckboxGroup({ options, legend }: CheckboxGroupProps) {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
+  console.log(selectedOptions);
+
+  const handleCheckboxChange = (option: string) => {
+    if (selectedOptions.includes(option)) {
+      // If the option is already selected, remove it from the array
+      setSelectedOptions(selectedOptions.filter((item) => item !== option));
+    } else {
+      // If the option is not selected, add it to the array
+      setSelectedOptions([...selectedOptions, option]);
+    }
+  };
+
+  return (
+    <div className="checkbox-wrap">
+      <legend className="checkbox-title">{legend}</legend>
+      {options.map((option) => (
+        <label key={option} className="container">
+          <input
+            className="checkbox"
+            type="checkbox"
+            value={option}
+            checked={selectedOptions.includes(option)}
+            onChange={() => handleCheckboxChange(option)}
+          />
+          <span className="checkmark" />
+          {option}
+        </label>
+      ))}
+    </div>
+  );
+}
+
+export default CheckboxGroup;
