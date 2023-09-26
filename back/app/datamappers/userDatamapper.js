798ac0fa74df048790b164dbd7ef80fd8a96768d
Ilias <ilias.yakdane@gmail.com>
2023-09-27 00:37:16 +0200

    add user in DB

--- back/app/datamappers/userDatamapper.js
+++ back/app/datamappers/userDatamapper.js
@@ -19,25 +19,16 @@ const userDatamapper = {
         try {
             const userToAdd = newUser;
             console.log(userToAdd);
-            // const query = `INSERT INTO "user"
-            //         ("firstname", "lastname", "email", "user_address", "user_password")
-            //         VALUES ($1, $2, $3, $4, $5)`;
-            // const {firstname, lastname, email, user_address, user_password} = userToAdd;
-            // const values = [firstname, lastname, email, user_address, user_password]
-            // const result = await client.query(query, values);
-            // console.log(result);
-            // return result;
+            const query = `INSERT INTO "user"
+                    ("firstname", "lastname", "email", "user_address", "user_password")
+                    VALUES ($1, $2, $3, $4, $5)`;
+            const {firstname, lastname, email, user_address, user_password} = userToAdd;
+            const values = [firstname, lastname, email, user_address, user_password]
+            const result = await client.query(query, values);
+            return result;
         } catch (error) {
             return error
         }
-        
-        // const query = `INSERT INTO "user"
-        //             ("firstname", "lastname", "email", "address", "password")
-        //             VALUES ($1, $2, $3, $4, $5)`;
-        // const values = [userToAdd.email];
-        // console.log(values);
-        // const result = await client.query(query, values);
-        // return result;
     },
 };
 
