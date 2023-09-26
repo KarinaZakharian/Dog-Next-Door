e63cd64a6f4d269cb300be44bb9b33c5af27a9e4
SniOpy <soufianeibna@gmail.com>
2023-09-26 17:01:17 +0200

    Init Cookie in login

--- back/app/controllers/userController.js
+++ back/app/controllers/userController.js
@@ -1,44 +1,38 @@
-const {userDatamapper} = require('../datamappers');
-const tokenController = require("../services/tokenController");
+const { userDatamapper } = require('../datamappers');
+const tokenController = require('../services/tokenController');
 
 const userController = {
-    checkUserInput : async (req,res,next) => {
-        const userFound = await userController.findUser(req, res);
-        console.log(req.body);
-        
-        const passwordUser = userFound.user_password;
-        if(passwordUser == req.body.user_password){
+  checkUserInput: async (req, res, next) => {
+    const userFound = await userController.findUser(req, res);
+    const passwordUser = userFound.user_password;
+    if (passwordUser == req.body.user_password) {
+      // req.session.user = userFound;
+      delete req.session.user_password;
+      const userToken = tokenController.createToken(userFound.id);
+      // req.cookie.accesstoken = userToken;
+      // req.session.user.token = userToken
 
-            // req.session.user = userFound;
-            delete req.session.user_password;
-            const userToken = tokenController.createToken(userFound.id);
-            // req.cookie.accesstoken = userToken;
-            // req.session.user.token = userToken
-            
-            res.json(userFound);
-        }else{
-            res.json("Couple identifiant mot de passe incorrect");
-            return;
-        }
-        // res.json(userFound);
-    },
-    
-    findUser : async (req,res,next) => {
-        const userInput = req.body;
-        try {
-            const user = await userDatamapper.getOneUser(userInput);
-            return user;
-        } catch (error) {
-            res.status(500).json(error.toString());
-        }
-       
-    },
+      res.json(userFound);
+    } else {
+      res.json('Couple identifiant mot de passe incorrect');
+      return;
+    }
+  },
 
-    // loggedUser : (req, res) => {
-    //     const userLogged = req.session.user;
-    //     res.json(userLogged);
-    // },
+  findUser: async (req, res, next) => {
+    const userInput = req.body;
+    try {
+      const user = await userDatamapper.getOneUser(userInput);
+      return user;
+    } catch (error) {
+      res.status(500).json(error.toString());
+    }
+  },
 
+  // loggedUser : (req, res) => {
+  //     const userLogged = req.session.user;
+  //     res.json(userLogged);
+  // },
 };
 
 module.exports = userController;
--- carnet_de_bord/Sprint_1.md
+++ carnet_de_bord/Sprint_1.md
@@ -6,7 +6,6 @@ Création de la branche develop_back
 
 ### Début du Sprint 1
 
-<<<<<<< HEAD
 ## J7 25/09
 
 - Début de séparation des tâches en back et front
@@ -16,11 +15,13 @@ Création de la branche develop_back
    - Création du fichier .sh pour lancer la création des roles et de la bdd, ainsi que l'import de la data
    - Mise en place de cors, qui va permettre de sécuriser le transfert de data entre le serveur et le navigateur
    - 
-=======
-# J7 22/09
-    - Rectification du cdc ( doublons et routes )
-    - Utiliser GitHub Project afin de partitioner les différentes tâches durant ce Sprint_1
-    - Réalisation des bases de L'API afin de commencer les premiers test avec le front
-    - 
 
->>>>>>> bf170c3d750bee543dfd2be4c5fa086944e6ad91
+
+## J8 - 26/09
+    
+    - Nous avons commencé la journée à travailler sur le retour de donnée en back pour le front,
+    - En back, nous avons de mettre en place les sessions en utilisant Redis.
+    - Toute l'équipe s'est réuni afin de faire les premiers tests entre le front et le back
+    - Après quelques erreurs, nous avons réussi à retourner les données via la BDD au front.
+    - Prochaine étape : on tente de finaliser la connexion, en ajoutant les erreurs en cas de mauvais information (email, password), l'insertion d'un utilisateur via le formulaire de contact,
+  et l'appararition de la page Account.
