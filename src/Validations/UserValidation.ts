c109e3bff8bcd5c094f6e621475502155152912e
SniOpy <soufianeibna@gmail.com>
2023-09-27 10:44:31 +0200

    Get new update

--- src/Validations/UserValidation.ts
+++ src/Validations/UserValidation.ts
@@ -4,7 +4,7 @@ import * as yup from 'yup';
 // eslint-disable-next-line import/prefer-default-export
 export const loginSchema = yup.object().shape({
   email: yup.string().email().required(),
-  user_password: yup.string().min(8).max(25).required(),
+  user_password: yup.string().min(4).max(25).required(),
 });
 
 export const emailSchema = yup.object().shape({
@@ -18,7 +18,7 @@ export const lastnameSchema = yup.object().shape({
   lastname: yup.string().trim().required(),
 });
 export const passwordSchema = yup.object().shape({
-  user_password: yup.string().min(8).max(25).required(),
+  user_password: yup.string().min(4).max(25).required(),
 });
 
 export const citySchema = yup.object().shape({
