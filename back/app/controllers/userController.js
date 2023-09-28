0ea0b3b5765ab8a12f9765843360a50f88abee0d
Ilias <ilias.yakdane@gmail.com>
2023-09-28 14:07:42 +0200

    Cookie functionnality added

--- back/app/controllers/userController.js
+++ back/app/controllers/userController.js
@@ -6,51 +6,62 @@ const bcrypt = require('bcrypt');
 
 const userController = {
     checkUserInput : async (req,res) => {
-        const userFound = await userController.findUser(req, res);
+
+        // Vérification token si utilisateur déjà connecté
+        if(req.userId){
+            const userId = req.userId;
+            const userConnected = await userDatamapper.getOneUserById(userId);
+            delete userConnected.user_password;
+            return res.status(200).json(userConnected);
+        }
+
+        // Recherche utilisateur via ses identifiants de connection
+        const userFound = await userController.findUserByEmail(req, res);
         
         if(!userFound){
-            res.json("Vous n'avez pas de compte")
-        };
-
-        const passwordUser = userFound.user_password;
-        if(passwordUser == req.body.password){
-            delete userFound.user_password;
-            const userToken = tokenController.createToken(userFound.id);
-            res
-            .cookie("access_token", userToken, {
-                httpOnly:true,
-                secure : process.env.NODE_ENV === "production",
-            })
-            .status(200)
-            .json(userFound);
-        }else{
-            res
-            .json("Couple identifiant mot de passe incorrect");
-            return;
+            res.status(401).json("Vous n'avez pas accès")
+            return
         }
+        
+        const passwordUserCheck = await bcrypt.compare(req.body.user_password, userFound.user_password);            if(passwordUserCheck){
+                delete userFound.user_password;
+                const userToken = tokenController.createToken(userFound.id);
+                res
+                .cookie("access_token", userToken, {
+                    httpOnly:false,
+                    secure : process.env.NODE_ENV === "production",
+                })
+                .status(200)
+                .json(userFound);
+            }else{
+                res
+                .json("Couple identifiant mot de passe incorrect");
+                return;
+            }
+        
     },
     
-    findUser : async (req,res) => {
+    findUserByEmail : async (req,res) => {
         const userInput = req.body;
         try {
-            const user = await userDatamapper.getOneUser(userInput);
+            const user = await userDatamapper.getOneUserByEmail(userInput);
             return user;
         } catch (error) {
             res.status(500).json(error.toString());
         }
-       
+        
     },
-
+    
     createUser : async (req,res) => {
-
+        
         const newUser =  req.body;
-            
+        
         try {
-
+            
             // Vérification de l'existence du compte
-            const userExist = await userDatamapper.getOneUser(newUser);
+            const userExist = await userDatamapper.getOneUserByEmail(newUser);
             if(userExist){
-                res.json("Cet email est déjà utilisé ! Veuillez vous logger")   
+                res.status(401).json("Cet email est déjà utilisé ! Veuillez vous logger")   
             };
             
             // Remplacement du mdp par un mdp crypté
@@ -60,10 +71,10 @@ const userController = {
             if(!validator.isEmail(newUser.email)){
                 res.json("Email invalide");
             };
-
+            
             // ^\d+\s[A-z\s\d]+,\s\d{5}\s[A-z\s]+$
             //! Vérification de la ville renseigné par l'utilisateur (JOI)
-
+            
             //! Validation de la longitude et lattitude via API Gouv
             const response = await userDatamapper.addUser(newUser);
             console.log(response);
@@ -72,15 +83,15 @@ const userController = {
         } catch (error) {
             res.status(404).json("erreur de connection server");
         }
-
-
+        
+        
     },
-
+    
     // loggedUser : (req, res) => {
     //     const userLogged = req.session.user;
     //     res.json(userLogged);
     // },
-
+    
 };
 
 module.exports = userController;
--- back/app/routers/userRouter.js
+++ back/app/routers/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const {userController} = require('../controllers/index.js');
 const userRouter= express.Router();
+const {checkTokenLogin} = require('../services/tokenController.js')
 
 // userRouter.post("/login",userController.findUser, userController.checkUserInput);
-userRouter.post("/login",userController.checkUserInput);
+userRouter.post("/login",checkTokenLogin, userController.checkUserInput);
 // userRouter.post("/account",userController.loggedUser);
 userRouter.post("/signup", userController.createUser);
 
