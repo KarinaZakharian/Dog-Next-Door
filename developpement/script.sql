18ec62d2fe3c12f706dee35783053f4ddcd48905
SniOpy <soufianeibna@gmail.com>
2023-09-28 18:34:33 +0200

    Ajout

--- developpement/script.sql
+++ developpement/script.sql
@@ -15,13 +15,17 @@ CREATE TABLE ANIMAL (
   feeding_schedule VARCHAR(42),
   potty_break_schedule VARCHAR(42),
   code_user VARCHAR(42),
-  PRIMARY KEY (code_animal)
+  PRIMARY KEY (code_animal),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE APPARTIENT (
   code_permission VARCHAR(42),
   code_role VARCHAR(42),
-  PRIMARY KEY (code_permission, code_role)
+  PRIMARY KEY (code_permission, code_role),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE BOOKING (
@@ -30,26 +34,34 @@ CREATE TABLE BOOKING (
   end_date VARCHAR(42),
   status VARCHAR(42),
   code_user VARCHAR(42),
-  PRIMARY KEY (code_booking)
+  PRIMARY KEY (code_booking),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE DETIENT (
   code_user VARCHAR(42),
   code_role VARCHAR(42),
-  PRIMARY KEY (code_user, code_role)
+  PRIMARY KEY (code_user, code_role),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE DISPONIBILITY (
   code_disponibility VARCHAR(42),
   start_date VARCHAR(42),
   end_date VARCHAR(42),
-  PRIMARY KEY (code_disponibility)
+  PRIMARY KEY (code_disponibility),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE ETABLIR (
   code_disponibility VARCHAR(42),
   code_user VARCHAR(42),
-  PRIMARY KEY (code_disponibility, code_user)
+  PRIMARY KEY (code_disponibility, code_user),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE IMAGE (
@@ -57,7 +69,9 @@ CREATE TABLE IMAGE (
   label VARCHAR(42),
   path_access VARCHAR(42),
   code_user VARCHAR(42),
-  PRIMARY KEY (code_image)
+  PRIMARY KEY (code_image),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE MESSAGE (
@@ -65,19 +79,25 @@ CREATE TABLE MESSAGE (
   topic VARCHAR(42),
   body VARCHAR(42),
   code_user VARCHAR(42),
-  PRIMARY KEY (code_message)
+  PRIMARY KEY (code_message),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE PERMISSION (
   code_permission VARCHAR(42),
   label VARCHAR(42),
-  PRIMARY KEY (code_permission)
+  PRIMARY KEY (code_permission),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE ROLE (
   code_role VARCHAR(42),
   label VARCHAR(42),
-  PRIMARY KEY (code_role)
+  PRIMARY KEY (code_role),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE TESTIMONIAL (
@@ -85,7 +105,9 @@ CREATE TABLE TESTIMONIAL (
   body VARCHAR(42),
   rating VARCHAR(42),
   code_user VARCHAR(42),
-  PRIMARY KEY (code_testimonial)
+  PRIMARY KEY (code_testimonial),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 CREATE TABLE USER (
@@ -99,7 +121,9 @@ CREATE TABLE USER (
   zip_code VARCHAR(42),
   town VARCHAR(42),
   country VARCHAR(42),
-  PRIMARY KEY (code_user)
+  PRIMARY KEY (code_user),
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME
 );
 
 ALTER TABLE ANIMAL ADD FOREIGN KEY (code_user) REFERENCES USER (code_user);
