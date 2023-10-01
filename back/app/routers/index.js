a47534015c4ec885b6da03cab2cbb326681a38b1
SniOpy <soufianeibna@gmail.com>
2023-10-01 14:28:21 +0200

    Try to fixed problem

--- back/app/routers/index.js
+++ back/app/routers/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const userRouter = require('./userRouter');
+const userRouter = require('./userRouter');
 
 userRouter.get("/", (req,res) => {
     res.send("API O'DogNextDoor");
 });
 
 router.use(userRouter);
+
+router.use(errorService);
 
 module.exports = router;
