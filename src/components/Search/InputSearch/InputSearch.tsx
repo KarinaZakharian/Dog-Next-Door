af9c9ea714c3a598bc22ddb32a39ffa80d099ece
David-Viau <viau.davidp@gamil.com>
2023-10-02 18:22:42 +0200

    Set-up css search-page

--- src/components/Search/InputSearch/InputSearch.tsx
+++ src/components/Search/InputSearch/InputSearch.tsx
@@ -1,17 +1,17 @@
-// form with several input fields for actualize the search
+import { DateRangePicker } from 'react-date-range';
+import Addresse from '../../InputType/Addresse/Addresse';
+import Button from '../../InputType/Button/Button';
+import DateSelect from '../../InputType/DatePiker/DateSelect';
+import DateRangeSelect from '../../InputType/DatePiker/DateRangeSelect';
 
-import { DateRangePicker } from "react-date-range";
-import Addresse from "../../InputType/Addresse/Addresse";
-import Button from "../../InputType/Button/Button";
-import DateSelect from "../../InputType/DatePiker/DateSelect";
-import DateRangeSelect from "../../InputType/DatePiker/DateRangeSelect";
+import './InputSearch.scss';
 
 function InputSearch() {
   return (
     <div className="inputSearch">
       <form>
         <Addresse style={undefined} />
-        <Button prop={"Recherche"} />
+        <Button prop={'Recherche'} />
       </form>
     </div>
   );
