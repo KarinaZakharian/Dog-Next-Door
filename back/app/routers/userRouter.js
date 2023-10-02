1965216eb8a7b158b3b6f6928af3dcccfa5eab59
SniOpy <soufianeibna@gmail.com>
2023-10-02 10:49:53 +0200

    Add comment to router

--- back/app/routers/userRouter.js
+++ back/app/routers/userRouter.js
@@ -7,14 +7,14 @@ const { checkTokenLogin, checkTokenMember } = require('../services/tokenControll
 const { controlWrapper:cw } = require('../services/errorService.js')
 const { checkSubscribeForm, checkLogInForm } = require('../services/validation/validationForm.js');
 
-
+//!Route GET
 userRouter.get("/login", (req,res) => {
     res.send("Login page")
 })
 userRouter.get("/subscribe", (req,res) => {
     res.send("Subscribe page")
 })
-
+//!Route POST
 userRouter.post("/login", checkTokenLogin, cw(userController.logInUser));
 
 userRouter.post("/subscribe", checkTokenLogin, cw(userController.createUser));
