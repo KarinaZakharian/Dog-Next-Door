0a550fa3c22e4a7e862d1635cf61c99e12be6013
KarinaZakharian <karina.zakharian@oclock.school>
2023-10-12 01:34:31 +0200

    animal-form cleanup and add comments

--- src/store/reducers/animal-form.ts
+++ src/store/reducers/animal-form.ts
@@ -1,62 +1,54 @@
-/* eslint-disable prettier/prettier */
 import {
   createAsyncThunk,
   createReducer,
   createAction,
 } from '@reduxjs/toolkit';
 
-import axios from 'axios';
 import axiosInstance from '../../utils/axios';
 
-
+// Define the initial state of the animal form
 interface AnimalState {
   message: string | null;
   error: unknown;
- 
- 
 }
 export const initialState: AnimalState = {
   message: null,
   error: null,
-  
-  
 };
 
+// Create an asynchronous thunk to handle form submission
 export const fillAnimalForm = createAsyncThunk(
   'animal/form',
   async (formData: FormData, thunkAPI) => {
     const objData = Object.fromEntries(formData);
     try {
       const data = await axiosInstance.post('/account/addanimal', objData);
-      return data   ;
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
   }
 );
-
+// Create an action for a successful form submission
 export const success = createAction('form/success ');
 
+// Define the animal form reducer
 const animalFormReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(fillAnimalForm.rejected, (state, action) => {
-      console.log('action rejected', action);
+      // Handle form submission rejection
+      //console.log('action rejected', action);
       state.error = action.payload.response.data.message;
       state.message = null;
-      
-      
     })
     .addCase(fillAnimalForm.fulfilled, (state, action) => {
-      // state.logged = true;
-      console.log('action fulfilled', action);
-      // state.firstname = action.payload.firstname;
+      // Handle a successful form submission
+      //console.log('action fulfilled', action);
       state.error = null;
       state.message = action.payload.data.message;
-     
-
-      // state.token = action.payload.token;
     })
     .addCase(success, (state) => {
+      // Reset the state when success action is dispatched
       state.error = null;
       state.message = null;
     });
