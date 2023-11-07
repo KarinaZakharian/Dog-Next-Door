750c7aea2ad9f816ec9f518e587b483e317f7d09
David-Viau <viau.davidp@gamil.com>
2023-11-07 11:26:11 +0100

    Delete back folder and petsitter sass

--- back/app/services/redisClient.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require('redis');
-
-// Initialisation du client redis
-let redisClient = createClient();
-redisClient.connect().catch(console.error);
-
-module.exports = redisClient;
--- back/app/services/validation/loginFormSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-const loginFormSchema = Joi.object({
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'fr', 'school'] } }).required(),
-    password: Joi.required()
-}).required();
-
-module.exports = loginFormSchema;
