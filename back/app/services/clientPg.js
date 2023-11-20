d8af38c505cb2dd8706f3be624055485fa5038a5
David-Viau <viau.davidp@gamil.com>
2023-11-20 14:27:18 +0100

    Added new back folder

--- back/app/services/clientPg.js
+++ back/app/services/clientPg.js
@@ -1,19 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 const client = new Pool({
-<<<<<<< HEAD
   user: process.env.PG_USER,
   host: process.env.PG_HOSTSERVER,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_DBPORT,
-=======
-    user: process.env.PG_USER,
-    host: process.env.PG_HOSTSERVER,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_DBPORT,
->>>>>>> 1d9cdbd (merge FixTsError and karina/end)
 });
 
 client.connect;
@@ -24,4 +16,3 @@ client.connect;
 module.exports = client;
 =======
 module.exports = client;
->>>>>>> 1d9cdbd (merge FixTsError and karina/end)
--- back/scripts/init_db.sql
+++ back/scripts/init_db.sql
@@ -1,8 +1,8 @@
--- On supprime la base de donnée SI ELLE EXISTE
-DROP DATABASE IF EXISTS odog;
 
 -- On supprime les rôles S'IL EXISTE
 DROP USER IF EXISTS odog;
+-- On supprime la base de donnée SI ELLE EXISTE
+DROP DATABASE IF EXISTS odog;
 
 -- Création du role et de la BDD
 
